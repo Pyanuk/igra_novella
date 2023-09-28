e5bdbbf367277ec9613c97f8ca579d9da72c3477
Pyanuk <144242312+Pyanuk@users.noreply.github.com>
2023-09-28 16:48:04 +0300

    Update Класс.py

--- Калькулятор.py
+++ Калькулятор.py
@@ -1,83 +1,27 @@
-import math
+#2код
+def rectangle_area(width, height):
+    return width * height
+width = 5
+height = 10
+area = rectangle_area(width, height)
+print(f"Площадь прямоугольника с шириной {width} и высотой {height} равна {area}.")
 
-
-def calculator():
-    print("Калькулятор")
-    print("Выберите номер операции:")
-    print("1. Сложение")
-    print("2. Вычитание")
-    print("3. Умножение")
-    print("4. Деление")
-    print("5. Возведение в степень")
-    print("6. Квадратный корень")
-    print("7. Факториал")
-    print("8. Синус")
-    print("9. Косинус")
-    print("10. Тангенс")
-    print("11. Выход")
-
-    operation = input("Введите номер операции: ")
-
-    if operation == "11":
-        print("Выход из программы")
-        return
-
-    elif operation in ['1', '2', '3', '4']:
-        pervoyechislo= float(input("Первое число:"))
-        vtoroyechislo= float(input("Второе число:"))
-
-        if operation == '1':
-            result = pervoyechislo + vtoroyechislo
-            print("Результат:", result)
-        elif operation == '2':
-            result = pervoyechislo - vtoroyechislo
-            print("Результат:",result)
-        elif operation == '3':
-            result = pervoyechislo * vtoroyechislo
-            print("Результат:",result)
-        elif operation == '4':
-            if vtoroyechislo == 0:
-                print("Результат:делить на ноль нельзя")
-            else:
-                result = pervoyechislo / vtoroyechislo
-                print("Результат:",result)
-    elif operation == '5':
-        pervoyechislo= float(input("Введите число: "))
-        stepen = float(input("Введите степень: "))
-        result = pervoyechislo ** stepen
-        print("Результат: ", result)
-    elif operation == '6':
-        number = float(input("Введите число: "))
-        if number < 0:
-            print("Ошибка: отрицательное число")
-        else:
-            result = math.sqrt(number)
-            print("Результат: ", result)
-    elif operation == '7':
-        number = int(input("Введите число: "))
-        result = 1
-        if number < 0:
-            print("Отрицательное число")
-        else:
-            for i in range(1, number + 1):
-                result *= i
-            print("Результат: ", result)
-    elif operation in ['8', '9', '10']:
-        angle = math.radians(float(input("Введите угол в градусах: ")))
-        if operation == '8':
-            result = math.sin(angle)
-            print("Результат: ", result)
-        elif operation == '9':
-            result = math.cos(angle)
-            print("Результат: ", result)
-        elif operation == '10':
-            result = math.tan(angle)
-            print("Результат: ", result)
+#3 код
+def is_even(number):
+    if number % 2 == 0:
+        return True
     else:
-        print("Некорректный номер операции")
+        return False
+result = is_even(2)
+print(result)
 
-    print("\n")
-    calculator()
+#4код
+def sum_digits(number):
+    total = 0
+    number_str = str(number)
+    for digit in number_str:
+        total += int(digit)
 
-
-calculator()
+    return total
+result = sum_digits(125)
+print(result)
